d71bb70ef1e388ed52f5acfba0e61d8fd6f68c7a
kibmuikia <muikiaa@gmail.com>
2019-01-08 02:30:20 +0300

    second iteration

--- vueapp-2/app.js
+++ vueapp-2/app.js
@@ -2,7 +2,24 @@ new Vue( {
 
   el: '#vApp',
   data: {
-    name: 'TheKibCoder'
+    name: 'TheKibCoder',
+    job: 'Lexion-Profiler',
+    website: 'https://google.co.ke',
+    age: 25,
+    x: 0,
+    y: 0
+  },
+  methods: {
+    greet( time ){
+      return 'Good ' + time +', ' + this.name;
+    },
+    ongeza(){
+      this.age++;
+    },
+    updateXY( event ){
+      this.x = event.offsetX;
+      this.y = event.offsetY;
+    }
   }
 
 } );
